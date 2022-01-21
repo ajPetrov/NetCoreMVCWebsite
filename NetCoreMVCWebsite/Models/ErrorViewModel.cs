f7e64bc0d8dc4f6ef65b15493c2af83b3336b604
antoanski11 <62160169+antoanski11@users.noreply.github.com>
2022-01-21 20:26:21 +0200

    Add project files.

---
+++ NetCoreMVCWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreMVCWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
